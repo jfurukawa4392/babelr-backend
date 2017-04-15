639ad3a5421722ef9e4bd1e561fb77af9987928a
Jesse Furukawa <tuc64377@temple.edu>
2017-04-15 11:15:27 -0700

    fix routing map

--- babelr/routing.py
+++ babelr/routing.py
@@ -19,5 +19,5 @@ class APIDemultiplexer(WebsocketDemultiplexer):
 channel_routing = [
     route("websocket.connect", consumers.ws_connect),
     route("websocket.receive", consumers.ws_receive),
-    route("websocket.rdisconnect", consumers.ws_disconnect)
+    route("websocket.disconnect", consumers.ws_disconnect)
 ]
